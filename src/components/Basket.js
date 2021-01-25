c8578f862a2990599c415964d42c689ab13b9536
saraswathi <saraswathiyadav20@gmail.com>
2021-01-25 13:14:26 +0530

    small chngs

---
+++ src/components/Basket.js
@@ -0,0 +1,56 @@
+import React from 'react';
+export default function Basket(props){
+ const{cartItems,onAdd,onRemove}=props;
+ const itemsPrice = cartItems.reduce((a,c) => a+c.price * c.qty,0);
+ const taxPrice = itemsPrice * 0.14;
+ const shippingPrice = itemsPrice > 2000 ? 0 :50;
+ const totalPrice = itemsPrice + taxPrice + shippingPrice;
+
+    return(
+     <aside className="block col-1">
+        <h2>Cart Items</h2>
+        <div>{cartItems.length === 0 && <div>cart is Empty</div>}</div>
+        {cartItems.map((item)=>(
+          <div key={item.id} className="row">
+              <div className="col-2">{item.name}</div>
+              <div className="col-2">
+                  <button onClick={()=>onAdd(item)} className="add">
+                     +
+                 </button>
+                  <button onClick={()=>onRemove(item)} className="remove"> 
+                    -
+                 </button>
+              </div>
+              <div className="col-2 text-right">
+              {item.qty} x ${item.price}
+              </div>
+          </div>  
+        ))}
+        {cartItems.length !==0 && (
+            <>
+            <hr></hr>
+            <div className="row">
+              <div className="col-2">Items price</div>  
+              <div className="col-1">${itemsPrice.toFixed(2)}</div>
+            </div>
+            <div className="row">
+              <div className="col-2">Tax price</div>  
+              <div className="col-1">${taxPrice.toFixed(2)}</div>
+            </div>
+            <div className="row">
+              <div className="col-2">Shipping price</div>  
+              <div className="col-1">${shippingPrice.toFixed(2)}</div>
+            </div>
+            <div className="row">
+              <div className="col-2"><strong>Total price</strong></div>  
+              <div className="col-1"> ${itemsPrice.toFixed(2)} </div>
+            </div>
+            <hr/>
+            <div className="row">
+                <button onClick={() => alert('Implement Checkout')}>Checkout</button>
+            </div>
+            </>
+        )}
+    </aside>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,26 @@
+const data={
+    products:[
+        {
+            id:'1',
+            name:'phone',
+            price:'25000',
+            image:'/images/phone.jpg',
+
+        },
+        {
+            id:'2',
+            name:'table',
+            price:'1500',
+            image:'/images/table.jpg',
+
+        },
+        {
+            id:'3',
+            name:'watch',
+            price:'2000',
+            image:'/images/watch.jpg',
+
+        },
+    ]
+}
+export default data
